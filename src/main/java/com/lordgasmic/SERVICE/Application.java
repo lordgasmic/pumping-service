155d35f452813c3f1d6d1435451f3016f1273d51
lordgasmic <wellman.nicholas@gmail.com>
2020-08-03 17:37:25 -0400

    Initial commit

---
+++ src/main/java/com/lordgasmic/SERVICE/Application.java
@@ -0,0 +1,12 @@
+package com.lordgasmic.==PACKAGE_NAME==;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ==APP_NAME==Application {
+
+    public static void main(String... args) {
+        SpringApplication.run(==APP_NAME==Application.class, args);
+    }
+}
