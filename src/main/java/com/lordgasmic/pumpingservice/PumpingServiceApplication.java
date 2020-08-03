9a012fe6ec3638a3f290a291a14baf6f7082d160
Nick Wellman <wellman.nicholas@gmail.com>
2020-08-03 17:44:23 -0400

    initial

--- src/main/java/com/lordgasmic/pumpingservice/PumpingServiceApplication.java
+++ src/main/java/com/lordgasmic/pumpingservice/PumpingServiceApplication.java
@@ -1,12 +1,12 @@
-package com.lordgasmic.==PACKAGE_NAME==;
+package com.lordgasmic.pumpingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ==APP_NAME==Application {
+public class PumpingServiceApplication {
 
     public static void main(String... args) {
-        SpringApplication.run(==APP_NAME==Application.class, args);
+        SpringApplication.run(PumpingServiceApplication.class, args);
     }
 }
