608caf653d75658449eee95c3d25acf6193d751f
Nick Wellman <wellman.nicholas@gmail.com>
2020-08-05 23:33:27 -0400

    add serialzable

--- src/main/java/com/lordgasmic/pumpingservice/entity/ExpressionEntityPk.java
+++ src/main/java/com/lordgasmic/pumpingservice/entity/ExpressionEntityPk.java
@@ -5,12 +5,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
+import java.io.Serializable;
 
 @Embeddable
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class ExpressionEntityPk {
+public class ExpressionEntityPk implements Serializable {
     private String date;
     private String timeExpressed;
 }
